9dee5513a5aaafb5ca66292b0a68117f2be4175e
Anirudh Tulasi <47075838+anirudhtulasi@users.noreply.github.com>
2023-06-14 15:14:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,75 @@
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import LeaveOneOut
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import NearestNeighbors
+import time
+def load_data(filename):
+    return np.loadtxt(filename)
 
-def load_data(file):
-    return pd.read_csv(file, sep='\s+', header=None)
+def leave_one_out_cross_validation(data, feature_set):
+    data_feature_set = data[:, feature_set]
+    num_instances = data.shape[0]
+    labels = data[:, 0]
+    correct_predictions = 0
+    
+    for i in range(num_instances):
+        #eucdistances
+        distances = np.sqrt(np.sum((data_feature_set - data_feature_set[i])**2, axis=1))
+        distances[i] = np.inf  #avoid same instance
+        nearest_neighbor_index = np.argmin(distances)
+        if labels[nearest_neighbor_index] == labels[i]:
+            correct_predictions += 1
 
-def leave_one_out_cross_validation(data, target):
-    loo = LeaveOneOut()
-    y_true = []
-    y_pred = []
-    for train_index, test_index in loo.split(data):
-        train = data.iloc[train_index]
-        test = data.iloc[test_index]
-        nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(train.drop(target, axis=1))
-        distances, indices = nbrs.kneighbors(test.drop(target, axis=1))
-        y_true.append(test[target].values[0])
-        y_pred.append(train.iloc[indices[0]][target].values[0])
-    return accuracy_score(y_true, y_pred)
+    return correct_predictions / num_instances
 
-def forward_selection(data, target):
-    features = list(data.columns)
-    features.remove(target)
+
+
+def forward_selection(data):
+    features = list(range(1, data.shape[1]))
     selected_features = []
-    print(f"Running nearest neighbor with all {len(features)} features, using \"leaving-one-out\" evaluation, I get accuracy of {leave_one_out_cross_validation(data, target)*100:.1f}%")
-    print("Beginning search.")
+    best_accuracy_so_far = 0
+    best_features_so_far = []
+
+    print(f"Running nearest neighbor with all {len(features)} features, using \"leaving-one-out\" evaluation, I get accuracy of {leave_one_out_cross_validation(data, features)*100:.1f}%\n")
+
     while len(features) > 0:
-        best_accuracy = 0
-        best_feature = None
+        best_accuracy_this_level = 0
+        best_feature_this_level = None
+
         for feature in features:
-            temp_features = selected_features + [feature]
-            accuracy = leave_one_out_cross_validation(data[temp_features + [target]], target)
-            print(f"Using feature(s) {temp_features} accuracy is {accuracy*100:.1f}%")
-            if accuracy > best_accuracy:
-                best_accuracy = accuracy
-                best_feature = feature
-        if best_feature:
-            selected_features.append(best_feature)
-            features.remove(best_feature)
-            print(f"Feature set {selected_features} was best, accuracy is {best_accuracy*100:.1f}%")
+            current_features = selected_features + [feature]
+            accuracy = leave_one_out_cross_validation(data, current_features)
+
+            print(f"Using feature(s) {current_features} accuracy is {accuracy*100:.1f}%")
+
+            if accuracy > best_accuracy_this_level:
+                best_accuracy_this_level = accuracy
+                best_feature_this_level = feature
+
+        if best_feature_this_level is not None:
+            selected_features.append(best_feature_this_level)
+            features.remove(best_feature_this_level)
+
+        print(f"Progress: {len(selected_features)}/{len(selected_features) + len(features)} features selected.\n")
+        if best_accuracy_this_level >= best_accuracy_so_far:
+            if best_accuracy_this_level > best_accuracy_so_far:
+                print(f"\nFeature set {selected_features} was best, accuracy is {best_accuracy_this_level*100:.1f}%\n")
+            best_accuracy_so_far = best_accuracy_this_level
+            best_features_so_far = selected_features.copy()
         else:
-            break
-    return selected_features
+            print("\n(Warning, Accuracy has decreased! Continuing search in case of local maxima)\n")
+
+    print(f"Finished search!! The best feature subset is {best_features_so_far}, which has an accuracy of {best_accuracy_so_far*100:.1f}%")
 
 def main():
     print("Welcome to Bertie Woosters Feature Selection Algorithm.")
     file = input("Type in the name of the file to test : ")
-    algorithm = int(input("Type the number of the algorithm you want to run.\n1) Forward Selection\n2) Backward Elimination\n"))
-
     data = load_data(file)
-    target = 0
-    if algorithm == 1:
-        print(f"This dataset has {data.shape[1] - 1} features (not including the class attribute), with {data.shape[0]} instances.")
-        print("Starting Forward Selection...")
-        selected_features = forward_selection(data, target)
-        print(f"Finished search!! The best feature subset is {selected_features}, which has an accuracy of {leave_one_out_cross_validation(data[selected_features + [target]], target)*100:.1f}%")
-    else:
-        print("Backward Elimination is not implemented in this script.")
+    start_time = time.time()
+    print(f"This dataset has {data.shape[1] - 1} features (not including the class attribute), with {data.shape[0]} instances.")
+    print("Starting Forward Selection...")
+    best_features = forward_selection(data)
+    #print(f"Finished search!! The best feature subset is {best_features}, which has an accuracy of {leave_one_out_cross_validation(data, best_features):.3f}")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"\nElapsed time: {elapsed_time} seconds")
 
 if __name__ == "__main__":
     main()
